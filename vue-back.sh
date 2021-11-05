6f433643f1f21e2dc70df195501160283b8893f9
“liujie-Leo” <ljdhxd@qq.com>
2021-11-05 17:12:28 +0800

    a

---
+++ vue-back.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+WORK_PATH='/home/projects/docker-front'
+cd $WORK_PATH
+
+echo "先清除老代码"
+git reset --hard origin/master
+git clean -f
+
+echo "拉取最新代码"
+git pull origin master
+
+echo "开始执行构建"
+docker build -t docker-back .
+
+echo "停止并删除旧容器"
+docker stop docker-back-container
+docker rm docker-back-container
+
+echo "启动新容器"
+docker container run -p 3000:3000 --name docker-back-container -d docker-back
