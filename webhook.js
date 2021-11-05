65efb61f97f83811d6bf7cecffe38cce389d3abc
“liujie-Leo” <ljdhxd@qq.com>
2021-11-05 16:14:40 +0800

    a

--- webhook.js
+++ webhook.js
@@ -1,5 +1,6 @@
 let http = require('http');
-let server = http.createServer((req,res) => {
+let server = http.createServer((req, res) => {
+  console.log(req.method,req.url)
   if (req.method == 'POST' && req.url == '/webhook') {
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify({ok:true}))
